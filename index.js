78f8f1a925a5f7ca8e40d01df66757d640a3c6d5
Auditi <auditibarua6@gmail.com>
2022-11-11 22:15:11 +0300

    added .env

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+//console.log(process.env.DB_USER)
+//console.log(process.env.DB_PASSWORD)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3njemyu.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+//console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function verifyJWT(req, res, next){
