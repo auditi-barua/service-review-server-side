fc4894212e1826ea371469d8f696e09f45c3c849
Auditi <auditibarua6@gmail.com>
2022-11-15 13:46:17 +0300

    added index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 
 //midleware
-
 
 
 app.use(cors());
@@ -17,51 +18,54 @@ app.use(express.json());
 //console.log(process.env.DB_USER)
 //console.log(process.env.DB_PASSWORD)
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3njemyu.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hpc3ria.mongodb.net/?retryWrites=true&w=majority`;
+
 //console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-function verifyJWT(req, res, next){
-  const authHeader = req.headers.authorization;
-
-  if(!authHeader){
-      return res.status(401).send({message: 'unauthorized access'});
-  }
-  const token = authHeader.split(' ')[1];
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
-      if(err){
-          return res.status(403).send({message: 'Forbidden access'});
-      }
-      req.decoded = decoded;
-      next();
-  })
-}
 
 
 const run = async () => {
   try {
 
-      const Services_Collection = client.db('review-site').collection('servises');
-      
+    const Services_Collection = client.db('review-site').collection('services');
+    const Review_Collection = client.db('review-site').collection('reviews');
 
-      app.get('/services', async (req, res) => {
-          const cursor = Services_Collection.find({});
-          const services = await cursor.toArray();
-          res.send(services);
-      });
+
+
+    app.get('/services', async (req, res) => {
+      const query = {}
+      const cursor = Services_Collection.find(query);
+      const services = await cursor.toArray();
+      console.log(services)
+      res.send(services);
+    });
+
+    app.get('/reviews', async (req, res) => {
+      const query = {}
+      const cursor = Review_Collection.find(query);
+      const reviews = await cursor.toArray();
+      // console.log(reviews);
+      res.send(reviews)
+    })
+
+    app.get('/myreviews', async (req, res) => {
+      const cursor = Review_Collection.find({ email: req.query.email });
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    })
 
   } finally {
-      // Ensures that the client will close when you finish/error
-      // await client.close();
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
 
 run().catch(console.dir);
 
 
-app.get("/",  (req, res) => {
-  res.send("service Api running");
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 });
 
 app.listen(port, () => {
